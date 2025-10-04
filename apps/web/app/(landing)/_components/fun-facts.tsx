1a0146ecf00246b556cc88b103f66cda8bcfe9ea
LukaszWojcik3B <wulkasz0@gmail.com>
2025-10-04 23:17:07 +0200

    karuzela

--- apps/web/app/(landing)/_components/fun-facts.tsx
+++ apps/web/app/(landing)/_components/fun-facts.tsx
@@ -1,34 +1,96 @@
 "use client";
 
-import { Card, CardContent, CardTitle } from "@hackathon/ui";
-import { useState } from "react";
+import {
+  Card,
+  CardContent,
+  CardTitle,
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@hackathon/ui";
+import { useRef } from "react";
 import type { LandingPageData } from "../_api/get-landing-data";
+
+import { TypographyP } from "@hackathon/ui";
 
 type Props = {
   initialData: LandingPageData;
 };
 
-export default function FunFacts({ initialData }: Props) {
-  const [randomNumber] = useState<number>(
-    Math.floor(Math.random() * initialData.facts.length),
+function getRandomNumber(factsNumber: number) {
+  return Math.floor(Math.random() * factsNumber);
+}
+
+function FunFactsCard({
+  fact,
+}: {
+  fact: { id: string; fact: string; source: string };
+}) {
+  return (
+    <Card className="m-5 bg-[#fefefe] p-5">
+      <CardTitle className="mb-5">Ciekawostka:</CardTitle>
+      <CardContent>
+        <ul>
+          <li key={fact.id}>
+            <TypographyP>{fact.fact}</TypographyP>
+            <br></br>
+            <em>źródło: {fact.source}</em>
+          </li>
+        </ul>
+      </CardContent>
+    </Card>
   );
-  const straightFacts = initialData.facts[randomNumber];
+}
+
+export default function FunFacts({ initialData }: Props) {
+  const intervalRef = useRef<NodeJS.Timeout | null>(null);
 
   return (
-    <div>
-      {straightFacts && (
-        <Card className="m-5 bg-[#fefefe] p-5">
-          <CardTitle className="mb-5">Ciekawostka:</CardTitle>
-          <CardContent>
-            <ul>
-              <li key={straightFacts.id}>
-                <strong>{straightFacts.fact}</strong>
-                <br></br>
-                <em>źródło: {straightFacts.source}</em>
-              </li>
-            </ul>
-          </CardContent>
-        </Card>
+    // <div>{/* {straightFacts && <FunFactsCard fact={straightFacts} />} */}</div>
+    <div className="w-full">
+      {initialData.facts[getRandomNumber(initialData.facts.length)] && (
+        <Carousel
+          opts={{
+            align: "start",
+            loop: true,
+          }}
+          className="relative w-full"
+          setApi={(api) => {
+            if (api && !intervalRef.current) {
+              intervalRef.current = setInterval(() => {
+                api.scrollNext();
+              }, 10000);
+            }
+          }}
+        >
+          <CarouselContent>
+            <CarouselItem>
+              <FunFactsCard
+                fact={
+                  initialData.facts[getRandomNumber(initialData.facts.length)]!
+                }
+              />
+            </CarouselItem>
+            <CarouselItem>
+              <FunFactsCard
+                fact={
+                  initialData.facts[getRandomNumber(initialData.facts.length)]!
+                }
+              />
+            </CarouselItem>
+            <CarouselItem>
+              <FunFactsCard
+                fact={
+                  initialData.facts[getRandomNumber(initialData.facts.length)]!
+                }
+              />
+            </CarouselItem>
+          </CarouselContent>
+          <CarouselPrevious className="absolute left-2 top-1/2 -translate-y-1/2" />
+          <CarouselNext className="absolute right-2 top-1/2 -translate-y-1/2" />
+        </Carousel>
       )}
     </div>
   );
