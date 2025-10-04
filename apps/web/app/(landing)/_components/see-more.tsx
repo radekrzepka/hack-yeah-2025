08075a248fa4770222f23840fa273a8c8f60abe3
LukaszWojcik3B <wulkasz0@gmail.com>
2025-10-04 21:56:53 +0200

    błagam

--- apps/web/app/(landing)/_components/see-more.tsx
+++ apps/web/app/(landing)/_components/see-more.tsx
@@ -1,7 +1,58 @@
-import { TypographyH1 } from "@hackathon/ui";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+  TypographyH3,
+  TypographyP,
+} from "@hackathon/ui";
+
+function SeeMoreCard({
+  title,
+  content,
+  imgPath,
+  imgAlt,
+}: {
+  title: string;
+  content: string;
+  imgPath: string;
+  imgAlt: string;
+}) {
+  return (
+    <Card className="flex">
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <TypographyP>{content}</TypographyP>
+        <img src={imgPath} alt={imgAlt} />
+      </CardContent>
+    </Card>
+  );
+}
 
 export function SeeMore() {
   return (
-    <TypographyH1>See more</TypographyH1>
+    <div className="m-5">
+      <TypographyH3>See more</TypographyH3>
+      <SeeMoreCard
+        title={"test"}
+        content={"lorem12"}
+        imgPath={"./"}
+        imgAlt={";);)"}
+      />
+      <SeeMoreCard
+        title={"test"}
+        content={"lorem12"}
+        imgPath={"./"}
+        imgAlt={";);)"}
+      />
+      <SeeMoreCard
+        title={"test"}
+        content={"lorem12"}
+        imgPath={"./"}
+        imgAlt={";);)"}
+      />
+    </div>
   );
 }
