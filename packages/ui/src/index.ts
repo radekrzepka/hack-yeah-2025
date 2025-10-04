85484a0eeea30d31769a17faedd2d2dd02e0c336
LukaszWojcik3B <wulkasz0@gmail.com>
2025-10-04 23:17:07 +0200

    karuzela

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -13,6 +13,7 @@ export * from "./components/ui/badge";
 export * from "./components/ui/breadcrumb";
 export * from "./components/ui/button";
 export * from "./components/ui/card";
+export * from "./components/ui/carousel";
 export * from "./components/ui/checkbox";
 export * from "./components/ui/collapsible";
 export * from "./components/ui/command";
@@ -39,3 +40,4 @@ export * from "./hooks/use-is-mobile";
 export * from "./hooks/use-stepper";
 
 export * from "./lib/utils";
+
