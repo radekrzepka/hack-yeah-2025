dd7df60d5691c2066159a5923b1e02781b27ba34
LukaszWojcik3B <wulkasz0@gmail.com>
2025-10-04 17:30:27 +0200

    upt

--- apps/web/app/(landing)/_components/header.tsx
+++ apps/web/app/(landing)/_components/header.tsx
@@ -2,16 +2,26 @@ import { Button } from "@hackathon/ui";
 
     export default function Header() {
         return (
-            <div className="bg-background h-1/4 mx-auto p-6 mt-20 flex">
-                <div className="w-1/2 px-6">
-                    <h1 className="text-2xl text-black w-fit  mx-auto py-6 flex ml-0 justify-center font-bold text-center">Myśl o jutrze, działaj dziś</h1>
-                    <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus obcaecati dolorem libero! Numquam cum rem sunt nobis nihil, excepturi at, doloremque omnis rerum voluptatum assumenda, odio quos ex magni reprehenderit!</p>
-                    <Button className="bg-white border-primary p-3 mt-5 text-primary border-x border-spacing-1 border-gray-300 hover:text-white shadow-lg ">Oblicz moją emeryturę</Button>
-                </div>
-                <div className="w-1/2 p-10 bg-accent rounded-2xl">
-                <p className="text-center">photo placeholder</p>
-                </div>
+          <div className="bg-background mx-auto mt-20 flex h-1/4 p-6">
+            <div className="w-1/2 px-6">
+              <h1 className="mx-auto ml-0 flex w-fit justify-center py-6 text-center text-2xl font-bold text-black">
+                Myśl o jutrze, działaj dziś
+              </h1>
+              <p className="">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+                obcaecati dolorem libero! Numquam cum rem sunt nobis nihil,
+                excepturi at, doloremque omnis rerum voluptatum assumenda, odio
+                quos ex magni reprehenderit!
+              </p>
+              <Button className="border-primary text-primary mt-5 border-spacing-1 border-x border-gray-300 bg-white p-3 shadow-lg hover:text-white">
+                Oblicz moją emeryturę
+              </Button>
             </div>
+            <div className="bg-accent w-1/2 rounded-2xl p-10">
+              <p className="text-center">photo placeholder</p>
+              <button></button>
+            </div>
+          </div>
         );
     }
     
