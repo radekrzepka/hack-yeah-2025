d6760f2d046a55ccd716c4271e3816d040f8ae09
Radosław Rzepka <radoslawrzepka02@gmail.com>
2025-10-04 21:43:47 +0200

    fix ?

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -50,7 +50,7 @@ async function bootstrap() {
   SwaggerModule.setup("api", app, document);
 
   const port = process.env.PORT || 4000;
-  await app.listen(port);
+  await app.listen(port, "0.0.0.0");
   console.log(`🚀 Application is running on port ${port}`);
 }
 
