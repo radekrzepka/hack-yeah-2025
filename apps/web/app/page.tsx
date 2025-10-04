8c032f2c820cb540b528fade343a2c8b69233e88
LukaszWojcik3B <wulkasz0@gmail.com>
2025-10-04 15:33:13 +0200

    working_branch

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,16 +1,10 @@
+import { Button } from "@hackathon/ui";
+
 export default async function Dashboard() {
   return (
-    <div className="bg-background mx-auto p-6">
-      <h1 className="mb-4 text-2xl font-bold">Hackathon Template 2k25</h1>
-      <p className="text-muted-foreground">
-        Witamy w aplikacji do prognozy emerytalnej.
-        <a
-          href="/form"
-          className="text-primary ml-1 underline hover:no-underline"
-        >
-          Przejdź do kalkulatora emerytalnego
-        </a>
-      </p>
+    <div className="w-max">
+      <div className="bg-background mx-auto p-6">Hackathon Template 2k25</div>
+      <Button className="w-23">Click me</Button>
     </div>
   );
 }
